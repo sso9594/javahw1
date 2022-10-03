d146bec1ffa9c21ac748489eab6511ce90e5d9b3
sso9594 <sso9594@naver.com>
2022-10-03 15:46:37 +0900

    Initial commit

---
+++ src/BookArray.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Book {
+    String title, author;
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
+public class BookArray {
+    public static void main(String[] args) {
+        Book2[] book = new Book2[2];
+        Scanner scanner = new Scanner(System.in);
+        for(int i=0; i<book.length; i++) {
+            System.out.print("제목>>");
+            String title = scanner.nextLine();
+            System.out.print("저자>>");
+            String author = scanner.nextLine();
+            book[i] = new Book2(title, author);
+        }
+        for(int i=0; i<book.length; i++)
+            System.out.print("(" + book[i].title + ", " + book[i].author + ")");
+        scanner.close();
+    }
+}
